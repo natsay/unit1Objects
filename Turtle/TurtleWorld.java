8b18bec17beb74ed67ffd9443ee6426c078c0413
natsay <nhtse@NN123-HA-22.HS.sd203.org>
2015-08-20 14:11:25 -0500

    Explore Turtles 8/20/15

--- Turtle/TurtleWorld.java
+++ Turtle/TurtleWorld.java
@@ -6,8 +6,123 @@ public class TurtleWorld
     {
         World turtleWorld = new World();
         Turtle turtle = new Turtle(turtleWorld);
-        turtle.setPenColor(Color.RED);
+        turtle.setPenColor(Color.BLUE);
         turtle.penDown();
         turtle.forward(50);
+        turtle.turn(90);
+        turtle.penDown();
+        turtle.forward(50);
+        turtle.turn(90);
+        turtle.penDown();
+        turtle.forward(50);
+        turtle.turn(90); turtle.penDown();
+        turtle.forward(50);
+        turtle.turn(90);
+        
+        turtle.penDown();
+        turtle.forward(60);
+        turtle.turn(90);
+        turtle.penDown();
+        turtle.forward(70);
+        turtle.turn(90);
+        turtle.penDown();
+        turtle.forward(80);
+        turtle.turn(90);
+        turtle.penDown();
+        turtle.forward(90);
+        turtle.turn(90);
+        
+        turtle.penDown();
+        turtle.forward(100);
+        turtle.turn(90);
+        turtle.penDown();
+        turtle.forward(110);
+        turtle.turn(90);
+        turtle.penDown();
+        turtle.forward(120);
+        turtle.turn(90);
+        turtle.penDown();
+        turtle.forward(140);
+        turtle.turn(90);
+        
+        turtle.penDown();
+        turtle.forward(150);
+        turtle.turn(90);
+        turtle.penDown();
+        turtle.forward(160);
+        turtle.turn(90);
+        turtle.penDown();
+        turtle.forward(170);
+        turtle.turn(90);
+        turtle.penDown();
+        turtle.forward(180);
+        turtle.turn(90);
+        
+        turtle.penDown();
+        turtle.forward(190);
+        turtle.turn(90);
+        turtle.penDown();
+        turtle.forward(200);
+        turtle.turn(90);
+        turtle.penDown();
+        turtle.forward(210);
+        turtle.turn(90);
+        turtle.penDown();
+        turtle.forward(220);
+        turtle.turn(90);
+        
+        turtle.penDown();
+        turtle.forward(190);
+        turtle.turn(90);
+        turtle.penDown();
+        turtle.forward(200);
+        turtle.turn(90);
+        turtle.penDown();
+        turtle.forward(210);
+        turtle.turn(90);
+        turtle.penDown();
+        turtle.forward(220);
+        turtle.turn(90);
+        
+        turtle.penDown();
+        turtle.forward(230);
+        turtle.turn(90);
+        turtle.penDown();
+        turtle.forward(240);
+        turtle.turn(90);
+        turtle.penDown();
+        turtle.forward(250);
+        turtle.turn(90);
+        turtle.penDown();
+        turtle.forward(260);
+        turtle.turn(90);
+        
+        turtle.penDown();
+        turtle.forward(2);
+        turtle.turn(90);
+        turtle.penDown();
+        turtle.forward(240);
+        turtle.turn(90);
+        turtle.penDown();
+        turtle.forward(250);
+        turtle.turn(90);
+        turtle.penDown();
+        turtle.forward(260);
+        turtle.turn(90);
+        
+        
+        turtle.penDown();
+        turtle.forward(230);
+        turtle.turn(90);
+        turtle.penDown();
+        turtle.forward(240);
+        turtle.turn(90);
+        turtle.penDown();
+        turtle.forward(250);
+        turtle.turn(90);
+        turtle.penDown();
+        turtle.forward(260);
+        turtle.turn(90);
+        
     }
 }
