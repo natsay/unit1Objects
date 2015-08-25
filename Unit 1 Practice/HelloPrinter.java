0615fa186c3090be5a21679840fa6406c3ca679e
natsay <nhtse@NN123-HA-22.HS.sd203.org>
2015-08-25 14:05:46 -0500

    8/25/15

--- Unit 1 Practice/HelloPrinter.java
+++ Unit 1 Practice/HelloPrinter.java
@@ -1,8 +1,38 @@
+// public class HelloPrinter
+// { public static void main( String[] args)
+//     {
+//         //display a greeting in the console window
+//         
+//         System.out.println("Hello, World");
+//     }
+// }
+
+//Chp 1: Synatax Errors
+
+// public class helloPrinter
+// { public static void main( String[] args)
+//     { 
+//         System.out.println("An emergency Broadcast"); 
+//     }
+// }   
+
+//Chp 1: Hellow 2 
+// import javax.swing.JOptionPane; 
+// public class HelloPrinter
+// { public static void main( String[] args)
+//     {String name=JOptionPane.showInputDialog("What is your name?"); 
+//      System.out.println(name);
+//     }
+// }   
+
+//Chp 1: Hello 3
+
+import java.net.UR:; 
+import javax.swing.ImageIcon; 
+import havax.swing.JOptionPane; 
+
 public class HelloPrinter
-{ public static void main( String[] args)
-    {
-        //display a greeting in the console window
-        
-        System.out.println("Hello, World");
-    }
-}
+{ public static void main( String[] args) throws Exception 
+    {URL imageLocation= new URL( 
+        "http://horstmann.com/java4everyone/duke.gif"); 
+     Joption    
