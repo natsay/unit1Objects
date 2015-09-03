40b9c3446fbe8c46426c31d8bfe69aaf475ee56d
natsay <nhtse@NN123-HA-22.HS.sd203.org>
2015-09-03 14:14:04 -0500

    CXCXVXV

--- TurtleLab/TurtleWorld.java
+++ TurtleLab/TurtleWorld.java
@@ -8,7 +8,7 @@ public class TurtleWorld
 { 
     public static void main(String args[]) 
     { 
-        World turtleWorld= new World(1000,1000); 
+        World turtleWorld= new World(4000,1000); 
         Turtle turtle= new Turtle(turtleWorld); 
         turtle.setPenColor(Color.GREEN);
         turtle.setShellColor(Color.BLUE);  
@@ -24,6 +24,12 @@ public class TurtleWorld
         Turtle turtle4= new Turtle(turtleWorld); 
         turtle4.setPenColor(Color.BLUE);
         turtle4.setShellColor(Color.YELLOW);
+        
+        Turtle turtle5= new Turtle(turtleWorld); 
+        turtle5.setPenColor(Color.ORANGE);
+        turtle5.setShellColor(Color.YELLOW);
+        
+        
        
         
         turtle.penDown();
@@ -32,21 +38,33 @@ public class TurtleWorld
         int num3= 0;
         int num4= 0;
         int num5= 0;
+        int num6=0;
         
-        while (num < 1000)
+        while (num < 100000)
         {
             turtle.forward(num);
             turtle2.forward(num2);
             turtle3.forward(num3);
             turtle4.forward(num4);
+            turtle5.forward(num5);
+            
+            
+        
+        
            
             turtle.turnRight();
-            turtle2.turnRight();
+            turtle2.turn(60);
             turtle3.turnRight();
-            turtle.forward(num3);
-            num += 1;
+            turtle4.turn(60);
+            turtle5.turnRight();
+            
+           
+            num+= 1;
             num2+=2;
             num3+=3;
+            num4+=4;
+            num5+=5;
+            
            
         }
             
--- TurtleLab/poopballsack.java
+++ TurtleLab/poopballsack.java
@@ -4,12 +4,12 @@ public class poopballsack
 { 
     public static void main(String args[]) 
     { 
-        World turtleWorld= new World(1000,1000); 
+        World turtleWorld= new World(2000,1000); 
         Turtle turtle= new Turtle(turtleWorld); 
         turtle.setPenColor(Color.GREEN);
         turtle.setShellColor(Color.BLUE);
         int num = 0;
-        while (num < 1000)
+        while (num < 100000)
         {   
            
             turtle.forward(num); 
